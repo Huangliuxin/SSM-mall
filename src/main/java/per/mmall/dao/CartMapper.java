1fa42721f77fbf0017c28243594c057b6033ed81
Huangliuxin <245079931@qq.com>
2018-06-16 22:00:46 +0800

    project init commit

---
+++ src/main/java/per/mmall/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package per.mmall.dao;
+
+import per.mmall.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ src/main/java/per/mmall/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package per.mmall.dao;
+
+import per.mmall.pojo.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
